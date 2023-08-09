ab84d93af5dc1ab58c9112a446dcf7cefad8b425
JeromeP93 <jeromespatrick@gmail.com>
2023-08-09 13:06:36 +0000

    task 8 complete

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    for char in str:
+        if ord('a') <= ord(char) <= ord('z'):
+            print(chr(ord(char) - ord('a') + ord('A')), end='')
+        else:
+            print(char, end='')
+    print()
