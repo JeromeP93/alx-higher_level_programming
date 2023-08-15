c29d75938e8914ecfedcbd65928386d33f06f23e
JeromeP93 <jeromespatrick@gmail.com>
2023-08-15 16:15:18 +0000

    task 4 complete

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,30 +1,9 @@
-#!/usr/bin/python3.8
-import dis
-import marshal
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
 
-# Function to extract names from the disassembled code
-
-
-def extract_names(code):
-    names = set()
-    for instruction in code:
-        if "argval" in instruction.argrepr:
-            name = instruction.argrepr.split()[1]
-            if not name.startswith("__"):
-                names.add(name)
-    return names
-
-
-# Read the compiled module
-with open("hidden_4.pyc", "rb") as f:
-    magic = f.read(4)
-    timestamp = f.read(4)
-    code = marshal.load(f)
-
-# Disassemble the code and extract names
-instructions = dis.get_instructions(code)
-names = extract_names(instructions)
-
-# Print names in alphabetical order
-for name in sorted(names):
-    print(name)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
