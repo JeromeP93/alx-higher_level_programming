7c456fb63a4ea7b1c654a1d8ca9c2c93e531d5e3
JeromeP93 <jeromespatrick@gmail.com>
2023-08-17 21:09:17 +0000

    task 0 complete

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,7 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -10,7 +10,8 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- */
+ *
+*/
 typedef struct dlistint_s
 {
 int n;
@@ -18,6 +19,7 @@ struct dlistint_s *prev;
 struct dlistint_s *next;
 } dlistint_t;
 
+/* Function prototypes */
 size_t print_dlistint(const dlistint_t *h);
 
 #endif /* LISTS_H */
