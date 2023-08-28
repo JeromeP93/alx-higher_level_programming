fc25153521e2d35e1b1c7002ad75a91315ff6102
JeromeP93 <jeromespatrick@gmail.com>
2023-08-28 13:03:56 +0000

    task 0 complete

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,5 +10,7 @@ def safe_print_list(my_list=[], x=0):
                 break
         print()
         return count
-    except:
-        return count
+    except KeyboardInterrupt:
+        pass
+    except Exception:
+        pass
