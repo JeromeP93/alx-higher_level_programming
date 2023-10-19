00999cb8cdb0e2e57aa0f978a4e28704c08b482d
Jerome S. Patrick <jeromespatrick@gmail.com>
2023-10-19 21:03:06 +0000

    Task 10 complete

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,6 +1,5 @@
--- Display all entries from the second_table with a score greater than or equal to 10 in my MySQL server.
+-- Display all entries from the second_table.
 -- The records are arranged in descending order based on their score.
 SELECT `score`, `name`
 FROM `second_table`
-WHERE `score` >= 10
 ORDER BY `score` DESC;
